c61e9275398791e46e6a6cc487b79c4ea6da8dab
Filipe Gomes <filipe.gomes.arante@gmail.com>
2026-08-28 11:07:02 -0300

    feat: update instance sizes in seeder

--- src/instance/instance_seeder.py
+++ src/instance/instance_seeder.py
@@ -5,7 +5,7 @@ class InstanceSeeder:
     def seed(self) -> None:
         SEED = 42
         MAX_VALUE = 1000
-        SIZES = [10, 100, 1000, 10000]
+        SIZES = [10, 20, 30, 40, 50, 100, 1000]
 
         for size in SIZES:
             instance_factory = InstanceFactory(max_value=MAX_VALUE, size=size, seed=SEED + size)
