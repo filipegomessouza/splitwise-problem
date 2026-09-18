18f4b2940a2e5747495afeaa0321effb2a53be12
Filipe Gomes <filipe.gomes.arante@gmail.com>
2026-09-17 21:28:09 -0300

    feat: read files with new format

--- src/instance/instance_reader.py
+++ src/instance/instance_reader.py
@@ -5,4 +5,13 @@ class InstanceReader:
     def read(self, file_path: str) -> Instance:
         balances = np.loadtxt(file_path, dtype=np.int64, ndmin=1)
 
-        return Instance(balances)
+        return Instance(balances, name=self._instance_name(file_path))
+
+    def read_with_index(self, file_path: str) -> Instance:
+        lines = np.loadtxt(file_path, dtype=np.int64, ndmin=2)
+        balances = lines[:, 1]
+
+        return Instance(balances, name=self._instance_name(file_path))
+
+    def _instance_name(self, file_path: str) -> str:
+        return file_path.split('/')[-1].split('.')[0]
